778182b60973fd9dad5213c73f4cde1fccb5d41b
岡田芽依 <imedakao@okadameinoMacBook-Air.local>
2025-06-03 18:18:39 +0900

    first commit

---
+++ book/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Book, Review
+#from .models import SampleModel
+
+admin.site.register(Book)
+admin.site.register(Review)
+#admin.site.register(SampleModel)
+# Register your models here.
