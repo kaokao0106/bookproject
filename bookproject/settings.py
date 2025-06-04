f16ac7f803bb2890a3eac743f897aee03d47b26b
岡田芽依 <imedakao@okadameinoMacBook-Air.local>
2025-06-04 14:02:15 +0900

    third

--- bookproject/settings.py
+++ bookproject/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import dj_database_url
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-+c5z#r0ts)9xy2w$ehtzqp(h4)7szu!-=@+qnho@uxqh1p)as9'
-
+SECRET_KEY = 'os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
